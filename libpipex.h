83c456215e61a3dc05a6bf05e8b6d9226e9a3972
mmClara42 <chagen@c4b3c1.42berlin.de>
2025-12-13 15:48:18 +0100

    first cleanup draft added

--- libpipex.h
+++ libpipex.h
@@ -1,13 +1,24 @@
 #ifndef LIBPIPEX_H
 # define LIBPIPEX_H
 
+/* data container used across the program */
 typedef struct s_data
 {
-	char	**arr;
-}			t_data;
+	char	**arr;/* argv/command split (owned) */
+	char	*path;/* constructed exec path (owned) */
+	int		infile_fd;/* input fd (-1 if unused) */
+	int		outfile_fd;/* output fd (-1 if unused) */
+	char	**envp;		/* environment pointer (NOT owned) */
+}	t_data;
+
+/*
+**	pid_t	*pids;
+**	size_t	n_children;
+*/
 
 char	**find_arg(char *cmd, char *append);
 char	*prefix_str(char *word, char prefix);
 char	*ft_strjoinjoin(char const *s1, char const *s2, char const *s3);
-void	cleanup_and_exit(int exit_code, const char *message, t_data *res);
+int		cleanup_and_return(int exit_code, const char *msg, t_data *res);
+void	cleanup_and_exit(int exit_code, const char *msg, t_data *res);
 #endif
--- src/cleanup.c
+++ src/cleanup.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 #include "../libpipex.h"
 
-void	cleanup_and_exit(int exit_code, const char *message, t_data *res)
+/* free owned resources but do NOT call exit */
+static void	cleanup_resources(t_data *res)
 {
-	if(res.arr)
+	if (!res)
+		return ;
+	if (res->arr)
 		ft_free_arr(res->arr);
-	if (message)
-		perror(message);
-	if (exit_code)
-		exit(exit_code);
-	else
-		return (0);
+	if (res->path)
+	{
+		free(res->path);
+		res->path = NULL;
+	}
+	// if (res->pids)
+	// {
+	// 	free(res->pids);
+	// 	res->pids = NULL;
+	// }
+	if (res->infile_fd > 2)
+	{
+		close(res->infile_fd);
+		res->infile_fd = -1;
+	}
+	if (res->outfile_fd > 2)
+	{
+		close(res->outfile_fd);
+		res->outfile_fd = -1;
+	}
+}
+
+/* cleanup and return an error code to caller (prefer in helpers) */
+int	cleanup_and_return(int exit_code, const char *msg, t_data *res)
+{
+	if (msg)
+		perror(msg);
+	cleanup_resources(res);
+	return (exit_code);
+}
+
+/* cleanup and exit the program (call from main only) */
+void	cleanup_and_exit(int exit_code, const char *msg, t_data *res)
+{
+	if (msg)
+		perror(msg);
+	cleanup_resources(res);
+	exit(exit_code);
 }
